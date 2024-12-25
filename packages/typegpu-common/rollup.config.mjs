001f07cca8261274819fd97e8a6f56bf09f23ba7
Iwo Plaza <iwoplaza@gmail.com>
2024-12-25 23:32:27 +0100

    Fixed building of packages.

---
+++ packages/typegpu-common/rollup.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import typegpu from 'rollup-plugin-typegpu';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+
+/**
+ * @type {import('rollup').RollupOptions}
+ */
+export default {
+  input: 'src/index.ts',
+  external: ['typegpu', /^typegpu\//],
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'esm',
+      sourcemap: true,
+    },
+    {
+      file: 'dist/index.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
+  plugins: [typescript(), typegpu(), terser()],
+};
